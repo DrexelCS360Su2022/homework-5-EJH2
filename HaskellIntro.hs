3394cb137fb1911e1004c5a1559e096ed8fcf4b5
Ethan <ethan.harriz@gmail.com>
2022-08-05 22:48:59 -0400

    Complete hw

--- HaskellIntro.hs
+++ HaskellIntro.hs
@@ -15,41 +15,71 @@ isThisWorking = "Yes"
 --
 
 lastDigit :: Integer -> Integer
-lastDigit = error "lastDigit not yet defined"
+lastDigit 0 = 0
+lastDigit n = n `mod` 10
 
 dropLastDigit :: Integer -> Integer
-dropLastDigit = error "dropLastDigit not yet defined"
+dropLastDigit 0 = 0
+dropLastDigit n = n `div` 10
 
 toDigits :: Integer -> [Integer]
-toDigits = error "toDigits not yet defined"
+toDigits 0 = []
+toDigits n | n < 0 = []
+           | otherwise = toDigits (dropLastDigit n) ++ [lastDigit n]
+
+doubleEveryOtherHelper :: [Integer] -> [Integer]
+doubleEveryOtherHelper [] = []
+doubleEveryOtherHelper [n] = [n]
+doubleEveryOtherHelper (x:s:xs) = x : 2 * s : doubleEveryOtherHelper xs
 
 doubleEveryOther :: [Integer] -> [Integer]
-doubleEveryOther = error "doubleEveryOther not yet defined"
+doubleEveryOther n = reverse (doubleEveryOtherHelper (reverse n))
 
 sumDigits :: [Integer] -> Integer
-sumDigits = error "sumDigits not yet defined"
+sumDigits [] = 0
+sumDigits [n] | n > 9     = lastDigit n + dropLastDigit n
+              | otherwise = n
+sumDigits (x:xs) = sumDigits [x] + sumDigits xs
 
 validate :: Integer -> Bool
-validate = error "validate not yet defined"
+validate n = lastDigit (sumDigits (doubleEveryOther (toDigits n))) == 0
 
 --
 -- Problem 2
 --
 
 pow :: (a -> a) -> Int -> a -> a
-pow = error "pow not yet defined"
+pow _ 0 = id
+pow f 1 = f
+pow f n = f . (pow f (n - 1))
 
 g :: Integer -> Integer
-g = error "g not yet defined"
+g 0 = 0
+g n = n - ((pow g 2) (n - 1))
 
 h :: Integer -> Integer
-h = error "h not yet defined"
+h 0 = 0
+h n = n - ((pow h 3) (n - 1))
 
 d :: Int -> Integer -> Integer
-d = error "d not yet defined"
+d _ 0 = 0
+d i n = n - ((pow (d i) i) (n - 1))
 
 --
 -- Problem 3
 --
 
-powerSet = error "powerSet not yet defined"
+getFirst :: Ord a => Set a -> a
+getFirst xs = (fst (Set.split xs))
+
+getRest :: Ord a => Set a -> Set a
+getRest xs = (snd (Set.split xs))
+
+powerSetHelper :: Ord a => Set a -> Set (Set a) -> Set (Set a)
+powerSetHelper xs ys | Set.isEmpty ys = (Set.singleton xs)
+                     | otherwise      = (Set.union (Set.singleton (Set.union xs (getFirst ys))) (powerSetHelper xs (getRest ys)))
+
+powerSet :: Ord a => Set a -> Set (Set a)
+powerSet xs | Set.isEmpty xs     = (Set.singleton Set.empty)
+            | (Set.size xs) == 1 = (Set.insert xs (Set.singleton Set.empty))
+            | otherwise          = (Set.union (powerSet (getRest xs)) (powerSetHelper (Set.singleton (getFirst xs)) (powerSet (getRest xs))))
--- README.md
+++ README.md
@@ -1,8 +1,8 @@
 # CS 360 Homework 3
 
-Name:  
+Name: Ethan Harriz
 
-Drexel Username:  
+Drexel Username: ejh336
 
 Put anything else you want us to know here.
 
@@ -10,8 +10,14 @@ Put anything else you want us to know here.
 
 Please let us know how much time you spent on each problem. You may enter time using any format described [here](https://github.com/wroberts/pytimeparse). Please put your time estimates *on the same line* as the problem label, not on the following line.
 
-Problem 1:  
+Problem 1: 20m
 
-Problem 2:  
+These problems weren't that difficult, building each part consecutively was quite helpful.
 
-Problem 3:  
+Problem 2: 2h
+
+Part 1 and Part 2 took a bit longer than I would have liked to have figured out, but I was massively overthinking Part 3 for way too long.
+
+Problem 3:  4h
+
+If I never see a powerset question again, I will die a *VERY* happy man
